ad3c88e7df367a6a4878c59f79a48a95012f094a
EricaReis <ericabeatrizdosreis@gmail.com>
2025-10-18 16:11:23 -0300

    Basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceptsModule } from 'src/concepts/concepts.module';
+import { ConceptsAutomaticModule } from 'src/concepts-automatic/concepts-automatic.module';
 
 @Module({
-  imports: [],
+  imports: [ConceptsModule, ConceptsAutomaticModule],
   controllers: [AppController],
   providers: [AppService],
 })
